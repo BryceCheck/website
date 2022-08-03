15a0307c0c41fb5c97319bdc0a7a36954d9560f5
Bryce Check <bryceacheck@gmail.com>
2022-08-03 10:00:30 -0500

    Changing navbar to row.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ function Navbar(props) {
   return (
       <div style={{position: 'absolute', top: '0', left: '0', width: '100vw'}}>
         <Container style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', maxHeight: '40px', color: 'white', backgroundColor: 'black'}} fluid>
-          <Row>
+          <Row style={{width: '100vw'}}>
             <Col sm='4' className='nav-col'><a className='nav-link' href='/'>Home</a></Col>
             <Col sm='4' className='nav-col'><a className='nav-link' href='/future'>Future</a></Col>
             <Col sm='4' className='nav-col'><a className='nav-link' href='/shop'>Shop</a></Col>
