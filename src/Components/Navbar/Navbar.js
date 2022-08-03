b15c5d0a0b83f212b2ede32bd1a047226aadc9bf
Ubuntu <ubuntu@ip-172-31-30-254.us-west-1.compute.internal>
2022-08-03 15:07:04 +0000

    Navbar is a row.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,14 +4,13 @@ function Navbar(props) {
   return (
       <div style={{position: 'absolute', top: '0', left: '0', width: '100vw'}}>
         <Container style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', maxHeight: '40px', color: 'white', backgroundColor: 'black'}} fluid>
-          <Row style={{display: 'flex', flexDirection: 'row', width: '100vw'}}>
+          <Row style={{display: 'flex', justifyContent: 'space-around', flexDirection: 'row', width: '100vw'}}>
             <Col sm='4' className='nav-col'><a className='nav-link' href='/'>Home</a></Col>
             <Col sm='4' className='nav-col'><a className='nav-link' href='/future'>Future</a></Col>
             <Col sm='4' className='nav-col'><a className='nav-link' href='/shop'>Shop</a></Col>
           </Row>
         </Container>
       </div>
-      
   )
 }
 
